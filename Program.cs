27cf71892eca434f156a0266dbc2c49b7853930e
Marcusjk9 <marcus.jakobsson@student.nbi-handelsakademin.se>
2025-09-26 14:47:23 +0200

    Written down what to do
    So that i can easily navigate every step without switching from program
    to website

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Ni ska ha arbetat aktivt med uppgiften
+Ni ska ha användt git aktivt under utvecklingsprocessen
+Ni ska dokumentera, med kommentarer i er kod, vad koden gör och varför ni
+gjort vissa val.
+Ni ska endast använda de koncept vi gått igenom hitils i kursen. Om ni undrar
+ifall ni får använda något annat, fråga utbildaren.
+Ni ska skriva, i en README.md fil, om hur man använder programmet och
+resonera kring era implementationsval. t.ex. varför ni användt eller inte
+användt komposition, inheritance, osv.
+Ni ska kunna svara på “Varför?” gällande er kod.
+*/
+
+/*
+    A user needs to be able to register an account  (__)
+    A user needs to be able to log out. (__)
+    A user needs to be able to log in. (__)
+    A user needs to be able to upload information about the item they wish to trade. (__)
+    A user needs to be able to browse a list of other users items. (__)
+    A user needs to be able to request a trade for other users items. (__)
+    A user needs to be able to browse trade requests. (__)
+    A user needs to be able to accept a trade request. (__)
+    A user needs to be able to deny a trade request. (__)
+    A user needs to be able to browse completed requests. (__)
+    */
+
+/*skapa user, sak att tradea, login/out, ska kunna hålla information om itemet, se andras inventory, trade request, browse trade request, accept, deny, browse historik*/
+
